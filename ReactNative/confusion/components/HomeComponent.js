d09372d2baa0915929dfc08eef355438d7fb59ae
Cyril Clement <dossiman@gmail.com>
2018-11-04 18:17:46 +0700

    react navigation part 3

--- ReactNative/confusion/components/HomeComponent.js
+++ ReactNative/confusion/components/HomeComponent.js
@@ -1,14 +1,47 @@
 import * as React from 'react';
-import { View, Text } from 'react-native';
+import { ScrollView, Text } from 'react-native';
+import { Card } from 'react-native-elements';
+import { DISHES } from '../shared/dishes';
+import { PROMOTIONS } from "../shared/promotions";
+import { LEADERS } from "../shared/leaders";
+
+function RenderItem({ item }) {
+  if (item != null) {
+    return (
+        <Card featuredTitle={item.name}
+            featuredSubtitle={item.designation}
+            image={require('./images/uthappizza.png')}>
+          <Text styke={{ margin: 10 }}>
+            {item.description}
+          </Text>
+        </Card>
+    );
+  } else {
+    return <View />;
+  }
+}
 
 class Home extends React.Component {
   static navigationOptions = {
     title: 'Home'
   };
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES,
+      promotions: PROMOTIONS,
+      leaders: LEADERS
+    };
+  }
+
   render() {
     return (
-        <View><Text>Home Component</Text></View>
+        <ScrollView>
+          <RenderItem item={this.state.dishes.filter(e => e.featured)[0]} />
+          <RenderItem item={this.state.promotions.filter(e => e.featured)[0]} />
+          <RenderItem item={this.state.leaders.filter(e => e.featured)[0]} />
+        </ScrollView>
     );
   }
 }
