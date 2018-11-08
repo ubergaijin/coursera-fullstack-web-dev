e22c574d69dbf21b257c46406d0182654820995b
Cyril Clement <dossiman@gmail.com>
2018-11-08 14:35:46 +0700

    react native animation

--- ReactNative/confusion/components/HomeComponent.js
+++ ReactNative/confusion/components/HomeComponent.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { View, ScrollView, Text } from 'react-native';
+import { View, Text, Animated, Easing } from 'react-native';
 import { Card } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
@@ -32,21 +32,69 @@ class Home extends React.Component {
     title: 'Home'
   };
 
-  render() {
-    return (
-        <ScrollView>
-          <RenderItem item={this.props.dishes.dishes.filter(e => e.featured)[0]}
-              isLoading={this.props.dishes.isLoading}
-              errMess={this.props.dishes.errMess} />
-          <RenderItem item={this.props.promotions.promotions.filter(e => e.featured)[0]}
-              isLoading={this.props.promotions.isLoading}
-              errMess={this.props.promotions.errMess} />
-          <RenderItem item={this.props.leaders.leaders.filter(e => e.featured)[0]}
-              isLoading={this.props.leaders.isLoading}
-              errMess={this.props.leaders.errMess} />
-        </ScrollView>
-    );
+  constructor(props) {
+    super(props);
+    this.animatedValue = new Animated.Value(0);
   }
-}
 
-export default connect(mapStateToProps)(Home);
+  componentDidMount() {
+    this.animate();
+  }
+
+  animate() {
+    this.animatedValue.setValue(0);
+    Animated.timing(
+        this.animatedValue,
+        {
+          toValue: 8,
+          duration: 8000,
+          easing: Easing.linear
+        }
+    ).start(() => this.animate());
+  }
+
+  render() {
+    const xpos1 = this.animatedValue.interpolate({
+      inputRange: [0, 1, 3, 5, 8],
+      outputRange: [1200, 600, 0, -600, -1200]
+    });
+
+    const xpos2 = this.animatedValue.interpolate({
+      inputRange: [0, 2, 4, 6, 8],
+      outputRange: [1200, 600, 0, -600, -1200]
+    });
+
+    const xpos3 = this.animatedValue.interpolate({
+      inputRange: [0, 3, 5, 7, 8],
+      outputRange: [1200, 600, 0, -600, -1200]
+    });
+
+    return (
+        <View style={{ flex: 1, flexDirection: 'row', justifyContent: 'center' }}>
+          <Animated.View style={{ width: '100%', transform: [{ translateX: xpos1 }] }}>
+            <RenderItem
+                item={this.props.dishes.dishes.filter(e => e.featured)[0]}
+                isLoading={this.props.dishes.isLoading}
+                errMess={this.props.dishes.errMess}
+            />
+          </Animated.View>
+          <Animated.View style={{ width: '100%', transform: [{ translateX: xpos2 }] }}>
+            <RenderItem
+                item={this.props.promotions.promotions.filter(e => e.featured)[0]}
+                isLoading={this.props.promotions.isLoading}
+                errMess={this.props.promotions.errMess}
+            />
+          </Animated.View>
+          <Animated.View style={{ width: '100%', transform: [{ translateX: xpos3 }] }}>
+            <RenderItem
+                item={this.props.leaders.leaders.filter(e => e.featured)[0]}
+                isLoading={this.props.leaders.isLoading}
+                errMess={this.props.leaders.errMess}
+            />
+          </Animated.View>
+        </View>
+  );
+  }
+  }
+
+  export default connect(mapStateToProps)(Home);
