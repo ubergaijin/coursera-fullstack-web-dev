668e87eb1ba2d5e83b9df85e6070240190312ce0
Cyril Clement <dossiman@gmail.com>
2018-11-12 16:17:03 +0700

    cookies

--- NodeJS/conFusionServer/app.js
+++ NodeJS/conFusionServer/app.js
@@ -32,22 +32,32 @@ app.set('view engine', 'pug');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(cookieParser('12345-67890-09867-54321'));
 
 app.use(function (req, res, next) {
-  console.log(req.headers);
+  console.log(req.signedCookies);
 
-  const authHeader = req.headers.authorization;
+  if (!req.signedCookies.user) {
+    const authHeader = req.headers.authorization;
 
-  if (!authHeader) {
-    res.setHeader('WWW-Authenticate', 'Basic');
-    next(createError(401, 'You are not authenticated!'));
+    if (!authHeader) {
+      res.setHeader('WWW-Authenticate', 'Basic');
+      next(createError(401, 'You are not authenticated!'));
+    } else {
+      const [username, password] = Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
+
+      if (username === 'admin' && password === 'password') {
+        res.cookie('user', 'admin', { signed: true });
+        next();
+      } else {
+        next(createError(401, `${username}:${password} is not an authorized user!`));
+      }
+    }
   } else {
-    const [username, password] = Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
-    if (username === 'admin' && password === 'password') {
+    if (req.signedCookies.user === 'admin') {
       next();
     } else {
-      next(createError(401, `${username}:${password} is not an authorized user!`));
+      next(createError(401, `${req.signedCookies.user} is not an authorized user!`))
     }
   }
 });
